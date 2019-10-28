87f3f8dc1df73569acf30a66b5b55bcc6666db11
Simon Rüegg <simon.ruegg@vshn.ch>
2019-10-28 09:11:19 +0100

    Initial setup of types

---
+++ api/v1alpha1/s3bucket_types.go
@@ -0,0 +1,103 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	runtimev1alpha1 "github.com/crossplaneio/crossplane-runtime/apis/core/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// S3BucketParameters define the desired state of a Cloudscale S3 Bucket
+// https://www.cloudscale.ch/en/api/v1#objects-users
+type S3BucketParameters struct {
+	// DisplayName specifies the name of an S3 bucket
+	DisplayName string `json:"display_name,omitempty"`
+
+	// Tags are optional key, value pairs to add to an S3 bucket
+	// +optional
+	Tags map[string]string `json:"tags,omitempty"`
+}
+
+// S3BucketSpec defines the desired state of S3Bucket
+type S3BucketSpec struct {
+	runtimev1alpha1.ResourceSpec `json:",inline"`
+	S3BucketParameters           `json:",inline"`
+}
+
+// S3BucketStatus defines the observed state of S3Bucket
+type S3BucketStatus struct {
+	Status string `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// S3Bucket is the Schema for the s3buckets API
+// +kubebuilder:printcolumn:name="CLASS",type="string",JSONPath=".spec.classRef.name"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:resource:scope=Cluster
+type S3Bucket struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   S3BucketSpec   `json:"spec,omitempty"`
+	Status S3BucketStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// S3BucketList contains a list of S3Bucket
+type S3BucketList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []S3Bucket `json:"items"`
+}
+
+// An S3BucketClassSpecTemplate is a template for the spec of a dynamically
+// provisioned S3Bucket.
+type S3BucketClassSpecTemplate struct {
+	runtimev1alpha1.ClassSpecTemplate `json:",inline"`
+	S3BucketParameters                `json:",inline"`
+}
+
+// +kubebuilder:object:root=true
+
+// An S3BucketClass is a resource class. It defines the desired spec of resource
+// claims that use it to dynamically provision a managed resource.
+// +kubebuilder:printcolumn:name="PROVIDER-REF",type="string",JSONPath=".specTemplate.providerRef.name"
+// +kubebuilder:printcolumn:name="RECLAIM-POLICY",type="string",JSONPath=".specTemplate.reclaimPolicy"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:resource:scope=Cluster
+type S3BucketClass struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// SpecTemplate is a template for the spec of a dynamically provisioned
+	// S3Bucket.
+	SpecTemplate S3BucketClassSpecTemplate `json:"specTemplate"`
+}
+
+// +kubebuilder:object:root=true
+
+// S3BucketClassList contains a list of cloud memorystore resource classes.
+type S3BucketClassList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []S3BucketClass `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&S3Bucket{}, &S3BucketList{})
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module git.vshn.net/syn/stack-cloudscale
+
+go 1.13
+
+require (
+	github.com/crossplaneio/crossplane-runtime v0.0.0-20191025043010-78072ef19dc5
+	k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
+	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
+	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
+	sigs.k8s.io/controller-runtime v0.2.2
+)
